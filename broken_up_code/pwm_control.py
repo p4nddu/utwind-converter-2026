ac7f37af1b0b09812e5e45d9dd6b2df83bcbb462
eleniPeth <164553200+eleniPeth@users.noreply.github.com>
2026-06-21 16:24:59 -0400

    pwm 2 ramp up/down

--- broken_up_code/pwm_control.py
+++ broken_up_code/pwm_control.py
@@ -46,28 +46,39 @@ class PwmController:
 
     def ramp_pwm(
         self,
-        start: float,
-        stop: float,
+        pwm_channel: int,
+        current_duty: float,
+        target_duty: float,
         ramp_time_s: float,
         steps: int,
     ) -> None:
         if steps <= 0:
-            self.set_pwm_duty(stop)
-            self.set_pwm_duty2(stop)
+            if pwm_channel == 0:
+                self.set_pwm_duty(target_duty)
+            elif pwm_channel == 1:
+                self.set_pwm_duty2(target_duty)
+            else:
+                raise ValueError("pwm_channel must be 0 or 1")
             return
 
         dt = ramp_time_s / steps
 
         for i in range(steps + 1):
             frac = i / steps
-            duty = start + (stop - start) * frac
-            self.set_pwm_duty(duty)
-            self.set_pwm_duty2(duty)
+            duty = current_duty + (target_duty - current_duty) * frac
+
+            if pwm_channel == 0:
+                self.set_pwm_duty(duty)
+            elif pwm_channel == 1:
+                self.set_pwm_duty2(duty)
+            else:
+                raise ValueError("pwm_channel must be 0 or 1")
 
             time.sleep(dt)
+        return duty
 
-    def run(self) -> None:
-        target_duty = max(0.0, min(1.0, self.pwm_cfg.duty_fraction))
+
+    def init_pwm(self):
 
         self.set_gd_enable(False)
         self.set_gd_enable2(False)
@@ -80,11 +91,27 @@ class PwmController:
         self.set_gd_enable(True)
         self.set_gd_enable2(True)
 
+    def run(self) -> None:
+        target_duty = max(0.0, min(1.0, self.pwm_cfg.duty_fraction))
+
+        self.init_pwm(self)
+        current_pwm1_duty = 0.0
+        current_pwm2_duty = 0.0
+        
         time.sleep(0.1)
 
-        self.ramp_pwm(
-            0.0,
-            target_duty,
-            self.pwm_cfg.ramp_time_s,
-            self.pwm_cfg.ramp_steps,
+        current_pwm1_duty = self.ramp_pwm(
+            pwm_channel=self.pins.pwm_bcm,
+            current_duty=current_pwm1_duty,
+            target_duty=target_duty,
+            ramp_time_s=self.pwm_cfg.ramp_time_s,
+            steps=self.pwm_cfg.ramp_steps,
+        )
+
+        current_pwm2_duty = self.ramp_pwm(
+            pwm_channel=self.pins.pwm_bcm2,
+            current_duty=current_pwm2_duty,
+            target_duty=target_duty,
+            ramp_time_s=self.pwm_cfg.ramp_time_s,
+            steps=self.pwm_cfg.ramp_steps,
         )
